14548e889753ff9ba1b1c3cfd463f3472127f903
Riko <riko.fluchel@gmail.com>
2025-06-08 11:47:44 -0400

    complete lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -5,15 +5,5 @@ class Anagram:
         self.word = word
     
     def match(self, match_list):
-        # ORIGINAL
-        # matches = []
-        # # loop through each item in match_list
-        # for item in match_list:
-        #     # check if sorted word letters and sorted item letters are the same
-        #     if sorted(list(self.word)) == sorted(list(item)):
-        #         # append item if true
-        #         matches.append(item)
-        # return matches
-
         return [item for item in match_list if sorted(list(item)) == sorted(list(self.word))]
              
